ecfd8331a0566ef2752bab12de979f00d1290d2a
Martin Molema <Martin.Molema@nhlstenden.com>
2025-02-03 12:00:05 +0100

    added I/O-redirection tests

---
+++ run-test.ps1
@@ -0,0 +1,15 @@
+echo "Running tests"
+$stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
+Start-Process -FilePath .\cmake-build-release\CppCalculator -RedirectStandardInput "test-input.txt" -RedirectStandardOutput "test-output.txt" -NoNewWindow -Wait
+$stopwatch.Stop()
+$stopwatch.ElapsedMilliseconds
+
+echo "Comparing output"
+cmd /c fc /L test-output.txt test-output-check.txt > diff.txt
+if ($?) {
+    Write-Host "De bestanden zijn identiek."
+    exit 0
+} else {
+    Write-Host "Er zijn verschillen gevonden!"
+    exit 1
+}
