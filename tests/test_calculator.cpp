b8173a7e75c1d8a9917b03545ba2c646bf28390a
Martin Molema <martin@molema.org>
2026-02-09 16:54:28 +0100

    even compacter test statements

--- tests/test_calculator.cpp
+++ tests/test_calculator.cpp
@@ -2,36 +2,34 @@
 #include "../Calculator.h"
 #include "../Parser.h"
 
-    TEST(CalculatorTest, Addition) {
-        /** Arrange */
-        Calculator calculator;
-
-        /** Act */
-        double result = calculator.compute(2.0, '+', 3.0)
-
-        /** Assert */
-        EXPECT_DOUBLE_EQ(result, 5.0);
-    }
-
-TEST(CalculatorTest, Subtraction) {
-        /** Arrange */
+TEST(CalculatorTest, Addition) {
+    /** Arrange */
     Calculator calculator;
 
-        /** Act + Assert */
+    /** Act */
+    double result = calculator.compute(2.0, '+', 3.0);
+
+    /** Assert */
+    EXPECT_DOUBLE_EQ(result, 5.0);
+}
+
+TEST(CalculatorTest, Subtraction) {
+    /** Arrange */
+    Calculator calculator;
+
+    /** Act + Assert */
     EXPECT_DOUBLE_EQ(calculator.compute(5.0, '-', 2.0), 3.0);
 }
 
 TEST(CalculatorTest, Multiplication) {
-    Calculator calculator;
-    EXPECT_DOUBLE_EQ(calculator.compute(3.0, '*', 4.0), 12.0);
+    /** Arrange, Act, Assert */
+    EXPECT_DOUBLE_EQ(Calculator().compute(3.0, '*', 4.0), 12.0);
 }
 
 TEST(CalculatorTest, Division) {
-    Calculator calculator;
-    EXPECT_DOUBLE_EQ(calculator.compute(10.0, '/', 2.0), 5.0);
+    EXPECT_DOUBLE_EQ(Calculator().compute(10.0, '/', 2.0), 5.0);
 }
 
 TEST(CalculatorTest, DivisionByZero) {
-    Calculator calculator;
-    EXPECT_THROW(calculator.compute(10.0, '/', 0.0), std::runtime_error);
+    EXPECT_THROW(Calculator().compute(10.0, '/', 0.0), std::runtime_error);
 }
